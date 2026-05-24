d252b71b858cd66cacbf43d16adbfd6d255471b3
unknown <diannycast@gmail.com>
2026-05-23 23:51:41 -0500

    constructores

--- src/model/Pelicula.java
+++ src/model/Pelicula.java
@@ -8,4 +8,16 @@ public class Pelicula   {
     private int visitas;
     private int idCategoria;
     private int idClasificacion;
+
+    public Pelicula(){
+    }
+    public Pelicula  (int nuevoid, String nuevoTitulo, String nuevoDirector, int nuevoAño, int nuevoMins, int nuevasVistas, int nuevaCategoria, int nuevaClasificacion){
+        id = nuevoid
+                titulo=nuevoTitulo
+                        director=nuevoDirector
+                                año=nuevoAño
+                                        duracionMins=nuevoMins
+                                                vistas=nuevasVistas
+
+    }
 }
