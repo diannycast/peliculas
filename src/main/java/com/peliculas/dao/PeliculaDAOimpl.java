615e4a1003ed5878f7fe7c598ef63f68ea8cafb5
unknown <diannycast@gmail.com>
2026-05-24 19:08:27 -0500

    peliculas por id

--- src/main/java/com/peliculas/dao/PeliculaDAOimpl.java
+++ src/main/java/com/peliculas/dao/PeliculaDAOimpl.java
@@ -4,6 +4,21 @@ import com.peliculas.model.Pelicula;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
-public class PeliculaDAOimpl {
+// conceccion de base de datos para hacer las operaciones
+public class PeliculaDAOimpl implements PeliculaDAO;
+// buscar peliculas por su id
+@Override
+public Pelicula buscarPorId (int id){
+    Pelicula pelicula = null;
+    String sql = "select * from pelicula where id = ?";
+    try {
+        PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement();
+        ps.setInt(1. id);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()){
+            pelicula = new Pelicula();
+            pelicula.setId(rs.getInt("id"));
+            pelicula.setTitulo(rs.getString("titulo"));
+        }
+    }
 }
