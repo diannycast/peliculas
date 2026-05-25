fe382ae059db5d852684409bee2e83dbed21c682
unknown <diannycast@gmail.com>
2026-05-24 22:16:15 -0500

    scanner y dao

---
+++ src/menu/menu.java
@@ -0,0 +1,15 @@
+package com.peliculas.menu;
+
+import com.peliculas.dao.PeliculaDAO;
+import com.peliculas.dao.PeliculaDAOimpl;
+import com.peliculas.model.Pelicula;
+import java.util.List;
+import java.util.Scanner;
+public class menu {
+    // crear el dao y scaner para leer lo que escribe el usuario
+    PeliculaDAO dao = new PeliculaDAOimpl();
+    Scanner scanner = new Scanner(System.in);
+
+
+
+}
